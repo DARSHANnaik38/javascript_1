399ff61ed6eb86af367d968a9bc8db8f626c1af0
naikd2988@gmail.com <naikd2988@gmail.com>
2024-07-13 22:31:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//  let a=Math.random()*100;
+//  a=Number.parseInt(a)
+//  let inp;
+//  let score=100;
+
+//  while(inp!=a){
+//     score=score-1;
+//     inp=prompt("enter the number:");
+//     if (inp==a){
+//     console.log("congrats you did it")
+//     console.log("you guess the actual number in $(100-score) chances")
+//  }
+//  else if(inp > a && inp < 100){
+//     console.log("number is greater")
+//  }
+//  else if(inp < a && inp > 100){
+//     console.log("number is lesserr")
+//  }
+//  else{
+//     console.log("enter number between 1 to 100")
+//  }
+// }
+
+let number=33
+let string =String(number)
+console.log(string);
+console.log(typeof string);
+
+let c=5
+let a=Boolean(c)
+console.log(a);
